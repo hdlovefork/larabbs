e390d67e22668bc357d9e4260ff7827715690059
dean <1602264241@qq.com>
2018-11-11 23:05:08 +0800

    添加JWT

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Models\User;
 
@@ -19,8 +20,8 @@ class AuthorizationsController extends Controller
             if ($code = $request->code) {
                 $response = $driver->getAccessTokenResponse($code);
                 $token = array_get($response, 'access_token');
-                if($type == 'weixin'){
-                    $driver->setOpenId(array_get($response,'openid'));
+                if ($type == 'weixin') {
+                    $driver->setOpenId(array_get($response, 'openid'));
                 }
             } else {
                 $token = $request->access_token;
@@ -56,6 +57,46 @@ class AuthorizationsController extends Controller
                 break;
         }
 
-        return $this->response->array(['token' => $user->id]);
+        $token = \Auth::guard('api')->fromUser($user);
+
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    public function store(AuthorizationRequest $request)
+    {
+        $username = $request->username;
+
+        filter_var($username, FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+
+        $credentials['password'] = $request->password;
+
+        if (!$token = \Auth::guard('api')->attempt($credentials)) {
+            return $this->response->errorUnauthorized('用户名或密码错误');
+        }
+
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function update()
+    {
+        $token = \Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+
+    public function destroy()
+    {
+        \Auth::guard('api')->logout();
+        return $this->response->noContent();
     }
 }
